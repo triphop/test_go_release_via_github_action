cc850131e96af8c31b7713c5a031cb846d2d3029
yanshoutong <yanshoutong@sina.cn>
2023-05-10 14:12:52 +0800

    add color
    Signed-off-by: yanshoutong <yanshoutong@sina.cn>

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/fatih/color"
+)
 
 func main() {
 	fmt.Println("Hi, DEV World! 😉")
-	fmt.println("should fail to compile")
+
+	// Print with default helper functions
+	color.Cyan("Prints text in cyan.")
+
+	// A newline will be appended automatically
+	color.Blue("Prints %s in blue.", "text")
+
+	// These are using the default foreground colors
+	color.Red("We have red")
+	color.Magenta("And many others ..")
 }
